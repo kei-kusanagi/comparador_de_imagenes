ecc1133a2625a1c92ffc101dce7495a382fc0354
kei-kusanagi <kei_kusanagi@outlook.com>
2023-09-08 19:01:37 -0600

    añadir mejoras

--- lib/main.dart
+++ lib/main.dart
@@ -47,69 +47,75 @@ class _CompareSliderAppState extends State<CompareSliderApp> {
 
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    // double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Image Compare Slider'),
         elevation: 1,
         centerTitle: false,
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Container(
-                    height: 250,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    clipBehavior: Clip.hardEdge,
-                    child: ImageCompareSlider(
-                      itemOne: imageAcontroller.text.isNotEmpty
-                          ? Image.network(imageAcontroller.text)
-                          : Image.asset(imageAl),
-                      itemTwo: imageBcontroller.text.isNotEmpty
-                          ? Image.network(imageBcontroller.text)
-                          : Image.asset(imageB),
-                    ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  height: screenHeight / 2,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: ImageCompareSlider(
+                    handleSize: const Size(20, 40),
+                    itemOne: imageAcontroller.text.isNotEmpty
+                        ? Image.network(
+                            imageAcontroller.text,
+                          )
+                        : Image.asset(imageAl),
+                    itemTwo: imageBcontroller.text.isNotEmpty
+                        ? Image.network(
+                            imageBcontroller.text,
+                          )
+                        : Image.asset(imageB),
                   ),
                 ),
-              ],
+              ),
             ),
-          ),
-          TextFormField(
-            controller: imageAcontroller,
-            decoration: const InputDecoration(
-              labelText: 'Imagen Izquierda',
+            TextFormField(
+              controller: imageAcontroller,
+              decoration: const InputDecoration(
+                labelText: 'Imagen Izquierda',
+              ),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                imageAl = imageAcontroller.text;
-              });
-            },
-            child: const Text('Enviar'),
-          ),
-          const SizedBox(height: 10),
-          TextFormField(
-            controller: imageBcontroller,
-            decoration: const InputDecoration(
-              labelText: 'Imagen Derecha',
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  imageAl = imageAcontroller.text;
+                });
+              },
+              child: const Text('Enviar'),
             ),
-          ),
-          ElevatedButton(
-            onPressed: () {
-              setState(() {
-                imageB = imageBcontroller.text;
-              });
-            },
-            child: const Text('Enviar'),
-          ),
-        ],
+            const SizedBox(height: 10),
+            TextFormField(
+              controller: imageBcontroller,
+              decoration: const InputDecoration(
+                labelText: 'Imagen Derecha',
+              ),
+            ),
+            const SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  imageB = imageBcontroller.text;
+                });
+              },
+              child: const Text('Enviar'),
+            ),
+          ],
+        ),
       ),
     );
   }
