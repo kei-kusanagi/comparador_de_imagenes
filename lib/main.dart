3ebaa9cb4cce0ef20c3e9e277e4ba0f5e182d09f
kei-kusanagi <kei_kusanagi@outlook.com>
2023-09-12 16:44:33 -0600

    fix image size

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class _MyAppState extends State<MyApp> {
       theme: ThemeData(
         useMaterial3: true,
         brightness: Brightness.dark,
-        colorSchemeSeed: Colors.red,
+        colorSchemeSeed: Colors.purple,
       ),
       home: const CompareSliderApp(),
     );
@@ -33,7 +33,7 @@ class CompareSliderApp extends StatefulWidget {
 }
 
 class _CompareSliderAppState extends State<CompareSliderApp> {
-  String imageAl = 'lib/asets/_Aether.jpg';
+  String imageA = 'lib/asets/_Aether.jpg';
   String imageB = 'lib/asets/_Lumine.jpg';
   final TextEditingController imageAcontroller = TextEditingController();
   final TextEditingController imageBcontroller = TextEditingController();
@@ -48,61 +48,68 @@ class _CompareSliderAppState extends State<CompareSliderApp> {
   @override
   Widget build(BuildContext context) {
     double screenHeight = MediaQuery.of(context).size.height;
-    // double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Image Compare Slider'),
-        elevation: 1,
+        elevation: 10,
         centerTitle: false,
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  height: screenHeight / 2,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  clipBehavior: Clip.hardEdge,
-                  child: ImageCompareSlider(
-                    handleSize: const Size(20, 40),
-                    itemOne: imageAcontroller.text.isNotEmpty
-                        ? Image.network(
-                            imageAcontroller.text,
-                          )
-                        : Image.asset(imageAl),
-                    itemTwo: imageBcontroller.text.isNotEmpty
-                        ? Image.network(
-                            imageBcontroller.text,
-                          )
-                        : Image.asset(imageB),
-                  ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Container(
+                height: screenHeight / 1.5,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                clipBehavior: Clip.hardEdge,
+                child: ImageCompareSlider(
+                  handleSize: const Size(20, 40),
+                  itemOne: imageAcontroller.text.isNotEmpty
+                      ? Image.network(
+                          imageAcontroller.text,
+                        )
+                      : Image.asset(imageA),
+                  itemOneBuilder: (child, context) =>
+                      IntrinsicHeight(child: child),
+                  itemTwo: imageBcontroller.text.isNotEmpty
+                      ? Image.network(
+                          imageBcontroller.text,
+                        )
+                      : Image.asset(imageB),
+                  itemTwoBuilder: (child, context) =>
+                      IntrinsicHeight(child: child),
                 ),
               ),
             ),
-            TextFormField(
-              controller: imageAcontroller,
-              decoration: const InputDecoration(
-                labelText: 'Imagen Izquierda',
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: TextFormField(
+                controller: imageAcontroller,
+                decoration: const InputDecoration(
+                  labelText: 'Imagen Izquierda',
+                ),
               ),
             ),
             const SizedBox(height: 10),
             ElevatedButton(
               onPressed: () {
                 setState(() {
-                  imageAl = imageAcontroller.text;
+                  imageA = imageAcontroller.text;
                 });
               },
               child: const Text('Enviar'),
             ),
             const SizedBox(height: 10),
-            TextFormField(
-              controller: imageBcontroller,
-              decoration: const InputDecoration(
-                labelText: 'Imagen Derecha',
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: TextFormField(
+                controller: imageBcontroller,
+                decoration: const InputDecoration(
+                  labelText: 'Imagen Derecha',
+                ),
               ),
             ),
             const SizedBox(height: 10),
